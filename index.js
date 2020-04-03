fade6665c86bf4d40dda01f539da515f2750e17a
allison_johnson <allison.cuttler@gmail.com>
2020-04-02 21:21:12 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+// multiply = (num1, num2) => num1 * num2
+
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2 
+const random = Math.floor(Math.random() * 10) + 1 
+const mod = 18 % 7
+const max = Math.max(10, 20)
